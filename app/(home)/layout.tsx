d5a37da8ecaf060cdf706ce74e264df49d3f9dd1
Shakur JR <128142507+Abdulahi143@users.noreply.github.com>
2024-04-07 01:55:47 +0200

    updated the layouts due to deploying errors 3.6

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
+export default function HomeLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
     return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
+      <div>
+        {children}
+      </div>
     )
   }
