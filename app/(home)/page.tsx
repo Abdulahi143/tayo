e465e2ab239dad13a2ab664867df7528f6092128
Shakur JR <128142507+Abdulahi143@users.noreply.github.com>
2024-04-06 18:03:18 +0200

    updated the layouts due to deploying errors 2.7

---
+++ app/(home)/page.tsx
@@ -0,0 +1,24 @@
+
+import PartnersSection from "../_components/PartnersSection";
+import AboutSection from "../_components/about/AboutSection";
+import ContactSection from "../_components/contact/ContactSection";
+import { CoursesSection } from "../_components/coureses/CoursesSection";
+import GallerySection from "../_components/gallery/GallerySection";
+import Hero from "../_components/hero/Hero";
+import ClientOnly from "../components/ClientOnly";
+
+
+const Home =  () => {
+  return (
+    <ClientOnly>
+      <Hero />
+      <CoursesSection />
+      <AboutSection />
+      <ContactSection />
+      <PartnersSection />
+      <GallerySection />
+    </ClientOnly>
+  );
+}
+
+export default Home;
