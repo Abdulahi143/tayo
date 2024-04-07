db37086f6b2f83f7b8227589d5941f66ccff3631
Shakur JR <128142507+Abdulahi143@users.noreply.github.com>
2024-04-07 02:11:45 +0200

    updated the layouts due to deploying errors 3.8

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+     <body className={`nunito.className bg-[#010922]`}>
         <ReactQueryProvider>
           <Navbar />
           <ClientOnly>
