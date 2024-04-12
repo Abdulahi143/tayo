aa64d0161d733b39e2e2c8a05568f7d1fcdeac74
Shakur JR <128142507+Abdulahi143@users.noreply.github.com>
2024-04-12 08:52:48 +0200

    updated the og stuff  added og in /slug 7.5

--- app/(home)/blog/[slug]/opengraph-image.tsx
+++ app/(home)/blog/[slug]/opengraph-image.tsx
@@ -1,119 +1,54 @@
-import { ogImageSchema } from '@/lib/og';
-import { ImageResponse } from 'next/og';
-
-export const runtime = 'edge';
-
-export const alt = 'About Acme';
+import { ImageResponse } from 'next/og'
+ 
+// Route segment config
+export const runtime = 'edge'
+ 
+// Image metadata
+export const alt = 'About Acme'
 export const size = {
   width: 1200,
   height: 630,
-};
-export const contentType = 'image/png';
-
-export const metadata = {
-  metadataBase: new URL('https://main--tayo-website-v2.netlify.app'),
-};
+}
  
-const interBold = fetch(
-  new URL("../../../../assets/fonts/Inter-Bold.ttf", import.meta.url)
-).then((res) => res.arrayBuffer());
-
-const image = fetch(
-  new URL("../../../../assets/logo_tayo.png", import.meta.url)
-).then((res) => res.arrayBuffer());
-
-export default async function Image({ params }: { params: { slug: string } }) {
-  const post = await fetch(`https://main--tayo-website-v2.netlify.app/blog/${params.slug}`).then((res) =>
-    res.json()
-  );
-
-  const fontBold = await interBold;
-
-  const url = new URL(params.slug);
-  const values = ogImageSchema.parse(Object.fromEntries(url.searchParams));
-  const heading =
-    values.heading.length > 140
-      ? `${values.heading.substring(0, 140)}...`
-      : values.heading;
-
-  const { mode } = values;
-  const paint = mode === "dark" ? "#fff" : "#000";
-
-  const fontSize = heading.length > 30 ? "70px" : "100px";
-
-  const imageData = await image;
-  const blob = new Blob([imageData]);
-  const imageUrl = URL.createObjectURL(blob);
+export const contentType = 'image/png'
+ 
+// Image generation
+export default async function Image() {
+  // Font
+  const interSemiBold = fetch(
+    new URL('"../../../../assets/fonts/Inter-Bold.ttf"', import.meta.url)
+  ).then((res) => res.arrayBuffer())
  
   return new ImageResponse(
-    (<div
-      tw="flex relative flex-col p-12 w-full h-full items-start"
-      style={{
-        color: paint,
-        background: mode === "dark" ? "black" : "white",
-      }}
-    >
+    (
+      // ImageResponse JSX element
       <div
-        tw="flex leading-[1.1] font-bold tracking-tighter items-center"
         style={{
-          fontFamily: "Inter",
-          fontWeight: "bolder",
-          marginLeft: "-3px",
-          fontSize: "32px",
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
-        <img width="64" height="64" src={imageUrl} />
-        <h1 tw="pl-4">James Shopland</h1>
+        About Acme
       </div>
-
-      <div tw="flex flex-col flex-1 ">
-        <div
-          tw="flex text-xl uppercase font-bold tracking-tight"
-          style={{ fontFamily: "Inter", fontWeight: "normal" }}
-        >
-          {values.type}
-        </div>
-        <div
-          tw="flex leading-[1.1] text-[80px] font-bold tracking-tighter"
-          style={{
-            fontFamily: "Inter",
-            fontWeight: "bolder",
-            marginLeft: "-3px",
-            fontSize,
-          }}
-        >
-          {heading}
-        </div>
-      </div>
-      <div tw="flex items-center w-full justify-between">
-        <div
-          tw="flex text-xl"
-          style={{ fontFamily: "Inter", fontWeight: "normal" }}
-        >
-          jamesshopland.com
-        </div>
-        <div
-          tw="flex items-center text-xl"
-          style={{ fontFamily: "Inter", fontWeight: "normal" }}
-        >
-          <img height="86" src={imageUrl} />
-        </div>
-      </div>
-    </div>
-
-     
     ),
+    // ImageResponse options
     {
-      width: 1200,
-      height: 630,
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
       fonts: [
         {
-          name: "Inter",
-          data: fontBold,
-          weight: 700,
-          style: "normal",
+          name: 'Inter',
+          data: await interSemiBold,
+          style: 'normal',
+          weight: 400,
         },
       ],
     }
-  );
+  )
 }
