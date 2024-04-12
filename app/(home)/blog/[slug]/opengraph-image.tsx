bbe98fc78da31d3212ab9ee14232849715a7e45f
Shakur JR <128142507+Abdulahi143@users.noreply.github.com>
2024-04-12 09:44:18 +0200

    updated the og stuff  added og in /slug 8.1

--- app/(home)/blog/[slug]/opengraph-image.tsx
+++ app/(home)/blog/[slug]/opengraph-image.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+// @ts-nocheck
+import { ogImageSchema } from '@/lib/og'
 import { ImageResponse } from 'next/og'
  
 // Route segment config
@@ -10,45 +14,102 @@ export const size = {
   height: 630,
 }
  
-export const contentType = 'image/png'
+const image = fetch(
+  new URL("../../../assets/logo_tayo.png", import.meta.url)
+).then((res) => res.arrayBuffer())
  
 // Image generation
-export default async function Image() {
+export default async function Image({ params }: { params: { slug: string } }) {
   // Font
   const interSemiBold = fetch(
     new URL('../../../../assets/fonts/Inter-SemiBold.ttf', import.meta.url)
   ).then((res) => res.arrayBuffer())
  
+
+  const url = new URL(params.slug)
+  const values = ogImageSchema.parse(Object.fromEntries(url.searchParams))
+  const heading =
+    values.heading.length > 140
+      ? `${values.heading.substring(0, 140)}...`
+      : values.heading
+
+  const { mode } = values
+  const paint = mode === "dark" ? "#fff" : "#000"
+  const imageData = await image
+  const fontSize = heading.length > 30 ? "70px" : "100px"
+  const fontBold = await interSemiBold
   return new ImageResponse(
     (
       // ImageResponse JSX element
       <div
-        style={{
-          fontSize: 128,
-          background: 'black',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        TAYO FÖRENING BORLÄNGE
-      </div>
+          tw="flex relative flex-col p-12 w-full h-full items-start"
+          style={{
+            color: paint,
+            background: mode === "dark" ? "black" : "white",
+          }}
+        >
+          <div
+            tw="flex leading-[1.1] font-bold tracking-tighter items-center"
+            style={{
+              fontFamily: "Inter",
+              fontWeight: "bolder",
+              marginLeft: "-3px",
+              fontSize: "32px",
+            }}
+          >
+            
+            <img width="64" height="64" src={imageData} />
+            <h1 tw="pl-4">Tayo Förening</h1>
+          </div>
+
+          <div tw="flex flex-col flex-1 ">
+            <div
+              tw="flex text-xl uppercase font-bold tracking-tight"
+              style={{ fontFamily: "Inter", fontWeight: "normal" }}
+            >
+              {values.type}
+            </div>
+            <div
+              tw="flex leading-[1.1] text-[80px] font-bold tracking-tighter"
+              style={{
+                fontFamily: "Inter",
+                fontWeight: "bolder",
+                marginLeft: "-3px",
+                fontSize,
+              }}
+            >
+              {heading}
+            </div>
+          </div>
+          <div tw="flex items-center w-full justify-between">
+            <div
+              tw="flex text-xl"
+              style={{ fontFamily: "Inter", fontWeight: "normal" }}
+            >
+              Tayoforening.se
+            </div>
+            <div
+              tw="flex items-center text-xl"
+              style={{ fontFamily: "Inter", fontWeight: "normal" }}
+            >
+              <img height="86" src={imageData} />
+            </div>
+          </div>
+        </div>
     ),
     // ImageResponse options
     {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
-      ...size,
+      width: 1200,
+      height: 630,
       fonts: [
         {
-          name: 'Inter',
-          data: await interSemiBold,
-          style: 'normal',
-          weight: 400,
+          name: "Sansit_Sawshed",
+          data: fontBold,
+          weight: 700,
+          style: "normal",
         },
       ],
     }
+
   )
 }
